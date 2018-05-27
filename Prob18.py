d9774b2381693ffae0ad5f0e185328e3d8ab34b2
JaPaBo <j.patrick.bourne@gmail.com>
2018-05-27 12:12:08 -0700

    Solved problems 18 and 67 using the same method.

---
+++ Prob18.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Solve Project Euler Problem 18 by importing from csv
+"""
+import csv
+import numpy as np
+import time
+ 
+start = time.time()
+
+list = []
+
+#opens CSV and reads nonsquare entries into list of lists of strings
+fh = open('prob18.csv', 'r')
+for line in fh:
+    list.append(line.strip().split(' '))
+
+vector = list[len(list)-2]
+
+#converts all strings to numbers
+for a in range(len(list)):
+    for b in range(len(list[a])):
+        list[a][b] = int(list[a][b])
+
+#starting from next to last row, determining maximum path by summing each array entry with larger of two "lower" array entries
+for c in range(len(list)-1):
+    e = 13 - c
+    for d in range(len(list[e])):
+        list[e][d] += max(list[e+1][d],list[e+1][d+1])
+
+#Calculating Solution Time
+elapsed = (time.time() - start)
+
+#Program output
+print("Elapsed time: %f" % elapsed)
+print("answer is: %f" % list[0][0])
+ 
--- Prob20.py
+++ Prob20.py
@@ -6,11 +6,15 @@ Created on Sat May 26 23:22:32 2018
 """
 
 import math
+import time
+ 
+start = time.time()
 tally = 0
 
 num = str(math.factorial(100))
 for i in num:
     tally += int(i)
-    
-print(tally)
-    
+print("start!")
+
+elapsed = (time.time() - start)
+print("Solution is %i.  Calculated in %f seconds" % (tally, elapsed))
